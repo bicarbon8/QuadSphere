dcf71055a1c29ad42438b16c7776fcf6e07fe8a8
Jason Holt Smith <bicarbon8@gmail.com>
2023-02-16 17:25:05 +0000

    remove grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stats } from '@react-three/drei'
 import { QuadMesh } from './components/shapes';
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
+import { Camera } from 'three';
 
 function App() {
+  const cameraRef = useRef<Camera>();
   return (
     <Fragment>
       <div className='absolute_full'>
         <Canvas>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <OrbitControls position={[2, 2, 2]} />
-            <gridHelper args={[10, 20]} />
+            <OrbitControls />
             <axesHelper args={[0.5]} />
             <QuadMesh position={[0, 0, 0]} radius={1} />
             <Stats />
