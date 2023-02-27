fc28b2d356ee2b639f75427e0dd0e62be0cff741
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-02-27 12:01:09 +0000

    adding module federation plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from "@originjs/vite-plugin-federation";
 
 const production = process.env.NODE_ENV==='production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+        name: 'QuadSphere',
+        filename: 'remoteEntry.js',
+        // Modules to expose
+        exposes: {
+          './QuadSphereModule': './src/App.tsx',
+        }
+    })
+  ],
   server: {
     hmr: {
       clientPort: process.env.CODESPACES ? 443 : undefined
