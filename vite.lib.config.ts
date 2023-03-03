02e17b4c6d361a330f6a759f067dd48750e1da10
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-03-03 15:58:52 +0000

    add types to package.json and fix main path

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'QuadSphere',
+      name: 'quadsphere',
       fileName: () => `index.js`
     },
     rollupOptions: {
