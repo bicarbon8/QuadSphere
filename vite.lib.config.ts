b6916be54d4fe1f1b0b49b6297b03f8627857dc1
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-03-03 15:55:25 +0000

    fixing packaging

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -26,6 +28,7 @@ export default defineConfig({
     }
   },
   plugins: [
-    react()
+    react(),
+    dts()
   ]
 })
