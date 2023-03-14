79ddcfebf514cf5fb4a4adab8aacb70fa6a1ad0c
Jason Holt Smith <jason.smith@verizonconnect.com>
2023-03-14 16:33:22 +0000

    fix console debug

--- src/components/quad-mesh.tsx
+++ src/components/quad-mesh.tsx
@@ -21,7 +21,7 @@ export const QuadMesh = forwardRef((props: QuadMeshProps, ref: ForwardedRef<Mesh
     }
     const [key, setKey] = useState<string>(geometry.quad.key);
     useEffect(() => {
-        console.debug('creating new QuadSphere Mesh!');
+        console.debug('creating new Quad Mesh!');
         setKey(geometry.quad.key);
     }, [geometry.quad.key]);
     return (
